ab2928f8991463dfc2b3f324c66b579dc56f6b78
Nikola Plejic <nikola@plejic.com>
2010-12-11 19:32:15 +0100

    updated model

--- rvcal/shifts/models.py
+++ rvcal/shifts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Shift(models.Model):
-  person = models.CharField(max_length=255)
+  person = models.OneToOneField(User)
+  comment = models.CharField(max_length=255, blank=True)
   date = models.DateField('Shift date')
