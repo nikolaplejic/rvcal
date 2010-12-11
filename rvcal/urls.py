db1742343e44dfb1553f23477d9f1f64496bd761
Nikola Plejic <nikola@plejic.com>
2010-12-11 20:00:34 +0100

    added preliminary URL patterns

--- rvcal/urls.py
+++ rvcal/urls.py
@@ -3,12 +3,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^rvcal/', include('rvcal.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^rvcal/', include('rvcal.shifts.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
