70984d377e890d61f23cca057e4f0c151c9a75c5
Nikola Plejic <nikola@plejic.com>
2010-12-11 21:49:14 +0100

    updated relationship between a user and a shift

--- rvcal/shifts/models.py
+++ rvcal/shifts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Shift(models.Model):
-  person = models.OneToOneField(User)
+  person = models.ForeignKey(User, related_name="shift_user")
   comment = models.CharField(max_length=255, blank=True)
   date = models.DateField('Shift date')
