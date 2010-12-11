3416e4ca5f2e38ee8ed485a304ef98b715d9247d
Nikola Plejic <nikola@plejic.com>
2010-12-11 20:05:42 +0100

    render index.html template in index

--- rvcal/shifts/views.py
+++ rvcal/shifts/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+  template = 'index.html'
+  context = {}
+  return render_to_response(template, context)
