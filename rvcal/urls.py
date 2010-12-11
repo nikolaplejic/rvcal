ae731d7aaf996d289c414a47d22cef4e3f406074
klemo <klemo@eee901.(none)>
2010-12-11 20:12:29 +0100

    serving static files

--- rvcal/urls.py
+++ rvcal/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from rvcal import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^rvcal/', include('rvcal.shifts.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:], 'serve',
+         {'document_root':settings.STATIC_ROOT, 'show_indexes':True}),
+            
+        (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 'serve',
+         {'document_root':settings.MEDIA_ROOT, 'show_indexes':True}),
+        )
