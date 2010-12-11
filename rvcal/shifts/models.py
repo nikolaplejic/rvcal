98c446e98ea8349d224e9a53b5a64c2fc23453e7
Nikola Plejic <nikola@plejic.com>
2010-12-11 21:53:22 +0100

    defined string representation of a shift

--- rvcal/shifts/models.py
+++ rvcal/shifts/models.py
@@ -5,3 +5,6 @@ class Shift(models.Model):
   person = models.ForeignKey(User, related_name="shift_user")
   comment = models.CharField(max_length=255, blank=True)
   date = models.DateField('Shift date')
+
+  def __unicode__(self):
+    return self.person.username + " - " + self.date.isoformat()
