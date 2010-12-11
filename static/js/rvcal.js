a3f39e6a93d492020923d3dd44219d3a3543b20b
Nikola Plejic <nikola@plejic.com>
2010-12-11 22:50:09 +0100

    selected date has a different background color

--- static/js/rvcal.js
+++ static/js/rvcal.js
@@ -4,19 +4,32 @@ $(function() {
 	var m = today.getMonth();
 	var y = today.getFullYear();
 
+	window.rvcal = { current: null };
+
   $("#calendar").fullCalendar({
-  	dayClick: function() {
+  	dayClick: function(d, ad, jse, v) {
+  		$("tbody > tr > .fc-state-default").css("background", "white");
+  		$(this).css("background", "#555");
+  		$("#id_date").val(d);
   		if ($("#entryform").css("display") == "none") {
+  		  window.rvcal.current = this;
   		  $("#entryform").slideDown();
   		} else {
-  		  $("#entryform").slideUp();
+  		  if (window.rvcal.current == this) {
+  		    $("#entryform").slideUp();
+  		    window.rvcal.current = this;
+  		  } else {
+  		    window.rvcal.current = this;
+  		  }
   		}
   	}, 
   	viewDisplay: function() {
   		$("tbody > tr > .fc-state-default").hover(function() {
 		    $(this).css("background", "#999");
 	    }, function() {
-		    $(this).css("background", "white");
+	    	if (this != window.rvcal.current) {
+		      $(this).css("background", "white");
+		    }
 	    });
   	},
   	editable: true, 
