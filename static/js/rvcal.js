82d86c78fc9102a1f6018498246fc5930159d09a
klemo <klemo@eee901.(none)>
2010-12-12 12:44:23 +0100

    js: refactored inline callbacks to separate functions

--- static/js/rvcal.js
+++ static/js/rvcal.js
@@ -1,49 +1,65 @@
+/*
+* Main rvcal js code.
+* Use http://www.jslint.com/ before commit!!
+*/
+
 $(function() {
-	var today = new Date();
-	var d = today.getDate();
-	var m = today.getMonth();
-	var y = today.getFullYear();
+  var today = new Date();
+  var d = today.getDate();
+  var m = today.getMonth();
+  var y = today.getFullYear();
 
-	window.rvcal = { current: null };
+  window.rvcal = { current: null };
 
+  /* callback when user selectts day in calendar */
+  var _dayClickCallback = function(d, ad, jse, v) {
+    $("tbody > tr > .fc-state-default").css("background", "white");
+    $(this).css("background", "#555");
+    $("#id_date").val(d);
+    if ($("#entryform").css("display") == "none") {
+      window.rvcal.current = this;
+      $("#entryform").slideDown();
+    } else {
+      if (window.rvcal.current == this) {
+        $("#entryform").slideUp();
+        window.rvcal.current = this;
+      } else {
+        window.rvcal.current = this;
+      }
+    }
+  };
+  
+  /* sets hovering on days in calendar */
+  var _setHoverOnDay = function() {
+    $("tbody > tr > .fc-state-default").hover(function() {
+      if (this != window.rvcal.current) {
+        $(this).css({ 
+              "background": "#999", 
+              "cursor": "pointer"
+            });
+      }
+     }, function() {
+      if (this != window.rvcal.current) {
+        $(this).css({ 
+              "background": "white", 
+              "cursor": "pointer"
+            });
+      }
+     });
+  };
+
+  /* calendar setup */
   $("#calendar").fullCalendar({
-  	dayClick: function(d, ad, jse, v) {
-  		$("tbody > tr > .fc-state-default").css("background", "white");
-  		$(this).css("background", "#555");
-  		$("#id_date").val(d);
-  		if ($("#entryform").css("display") == "none") {
-  		  window.rvcal.current = this;
-  		  $("#entryform").slideDown();
-  		} else {
-  		  if (window.rvcal.current == this) {
-  		    $("#entryform").slideUp();
-  		    window.rvcal.current = this;
-  		  } else {
-  		    window.rvcal.current = this;
-  		  }
-  		}
-  	}, 
-  	viewDisplay: function() {
-  		$("tbody > tr > .fc-state-default").hover(function() {
-		    if (this != window.rvcal.current) {
-		      $(this).css({ 
-		    	  "background": "#999", 
-		    	  "cursor": "pointer"
-		      });
-		    }
-	    }, function() {
-	    	if (this != window.rvcal.current) {
-		      $(this).css({ 
-		    	  "background": "white", 
-		    	  "cursor": "pointer"
-		      });
-		    }
-	    });
-  	},
-  	editable: true, 
-  	eventSources: [
-  		'/rvcal/events/',
-  	],
-  	height: 400,
+   dayClick: function(d, ad, jse, v) {
+      _dayClickCallback.apply(this, [d, ad, jse, v]);
+   }, 
+   viewDisplay: function() {
+      _setHoverOnDay.apply(this);
+   },
+   editable: true, 
+   eventSources: [
+    '/rvcal/events/'
+   ],
+   height: 300
   });
 });
