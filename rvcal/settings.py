188d71078ab50689ccfaf0586a7dd880a9afab54
Nikola Plejic <nikola@plejic.com>
2011-01-12 15:52:58 +0100

    initial ldap config

--- rvcal/settings.py
+++ rvcal/settings.py
@@ -1,4 +1,6 @@
 # Django settings for rvcal project.
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 from os.path import dirname, join, abspath, pathsep
 
 DEBUG = True
@@ -102,3 +104,43 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+# Keep ModelBackend around for per-user permissions and maybe a local
+# superuser.
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = "ldap://bljak.org"
+
+#AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=bljak,dc=org",
+#    ldap.SCOPE_SUBTREE, "uid=%(user)s")
+AUTH_LDAP_USER_DN_TEMPLATE = "uid=%s,ou=People,dc=bljak,dc=org"
+
+# Set up the basic group parameters.
+#AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=django,ou=groups,dc=example,dc=com",
+#    ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)"
+#)
+#AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")
+
+# Only users in this group can log in.
+#AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
+
+# Populate the Django user from the LDAP directory.
+#AUTH_LDAP_USER_ATTR_MAP = {
+#    "first_name": "givenName",
+#    "last_name": "sn",
+#    "email": "mail"
+#}
+
+# Cache group memberships for an hour to minimize LDAP traffic
+AUTH_LDAP_CACHE_GROUPS = True
+AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
+
+import logging
+
+logger = logging.getLogger('django_auth_ldap')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
