ab6a578d8357a27261a2f9b22ea5b7dd3631b8b7
Nikola Plejic <nikola@plejic.com>
2011-08-20 22:05:12 +0200

    functioning user login

--- rvcal/shifts/urls.py
+++ rvcal/shifts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('shifts.views',
   (r'index/', 'index'), 
   (r'events/', 'events'), 
+  (r'login/', 'user_login'), 
 )
--- rvcal/shifts/views.py
+++ rvcal/shifts/views.py
@@ -3,12 +3,35 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.utils import simplejson
 from django.core.serializers.json import DjangoJSONEncoder
+from django.contrib.auth import authenticate, login, logout
 from forms import ShiftForm
 from models import Shift
 from datetime import datetime
 
+# user login
+def user_login(request):
+  context = {}
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request, user)
+        return HttpResponseRedirect(reverse(index))
+      else:
+        return HttpResponseRedirect(reverse(user_login))
+    else:
+      return HttpResponseRedirect(reverse(user_login))
+  else:
+    template = 'login.html'
+    return render_to_response(template, context)
+
 # serves main page with calendar
 def index(request):
+  if not request.user.is_authenticated():
+    return HttpResponseRedirect(reverse(user_login))
+
   context = {}
   if request.method == 'POST':
     form = ShiftForm(request.POST)
